d40ce243ce501d160f7034ae41a27d0fa0cd7898
Uffa Butt <uffa.butt@gmail.com>
2018-05-31 11:05:55 -0400

    Lab 4

---
+++ Lab4/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  subtitle: string = 'This is a subtitle';
+  altText: string = 'Angular Shield';
+}
